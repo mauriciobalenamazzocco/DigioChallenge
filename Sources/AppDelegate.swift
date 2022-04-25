6106994a54b719de6e1a64214a7336cd0e4ec718
Daniel Barreto <mauriciomazzocco@gmail.com>
2022-04-25 10:00:02 -0300

    Remove comments

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -16,19 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
 
-        // create the main navigation controller to be used for our app
-        let navController = UINavigationController()
+         let navController = UINavigationController()
 
-        // send that into our coordinator so that it can display view controllers
         coordinator = MainCoordinator(
             navigationController: navController,
             configuration: Configuration()
         )
 
-        // tell the coordinator to take over control
         coordinator?.start()
 
-        // create a basic UIWindow and activate it
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
